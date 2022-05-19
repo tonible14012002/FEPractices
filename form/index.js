d1d4624115de8599515f5a69fe83261cefb5e7c6
tonible14012002 <namanhble14012002@gmail.com>
2022-05-20 01:03:30 +0700

    fix animation

--- form/index.js
+++ form/index.js
@@ -3,54 +3,32 @@ const multiStepForm =
     document.querySelector("form[data-multi-step]");
 const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
 
-let currentStep = formSteps.findIndex(step=>{
-    return step.classList.contains("active");
-})
-
-formSteps.forEach(e=>{
-    e.classList.add('deactive')
-})
+let currentStep = getActiveCard()
 
 if (currentStep < 0){
     currentStep = 0;
     formSteps[currentStep].classList.add('active')
-    formSteps[currentStep].classList.remove('deactive')
 }
 
 formSteps.forEach(step=>{
     step.addEventListener("click", e=>{
         if (e.target.matches("[data-next]")){
             e.preventDefault()
-            if (e.target.classList.contains('deactive'))
-                return;
-            else {
-              formSteps[currentStep].classList.remove("active") 
-            formSteps[currentStep].classList.add('deactive')
-            formSteps[currentStep+1].classList.add('active')   
-            formSteps[currentStep+1].classList.remove('deactive')  
-            lessTheOppacity(formSteps[currentStep])
-            increaseTheOppacity(formSteps[currentStep+1])
+            
+            formSteps[currentStep].classList.remove("active")
+            Fade(currentStep)
             currentStep += 1;
             renderStep()  
-            }
             
         }
         else if (e.target.matches("[data-previous]")){
             e.preventDefault()
-            if (e.target.classList.contains('deactive'))
-                return;
-            else{
-                formSteps[currentStep].classList.remove("active")
-                formSteps[currentStep].classList.add('deactive')
-                formSteps[currentStep-1].classList.add('active')
-                formSteps[currentStep-1].classList.remove('deactive')  
-                lessTheOppacity(formSteps[currentStep])
-                increaseTheOppacity(formSteps[currentStep-1])
-                currentStep -= 1;
-                renderStep()
-            }
-            
-            
+
+            formSteps[currentStep].classList.remove("active")
+            Fade(currentStep)
+            currentStep -= 1;
+            renderStep()
+
         }
     })
 })
@@ -59,37 +37,24 @@ function renderStep(){
     if (currentStep < 0){
         currentStep = 0;
     }
-    if (currentStep > STEP_COUNT){
-        currentStep = 3;
+    if (currentStep >= STEP_COUNT){
+        currentStep = STEP_COUNT-1;
     }
-    formSteps[currentStep].classList.add('active')
-    formSteps[currentStep].classList.remove('deactivate')
-}
-
-
-function lessTheOppacity(node)
-{
-    var oppArray = ["0.9", "0.8", "0.7", "0.6", "0.5", "0.4", "0.3", "0.2", "0.1", "0"];
-    var x = 0;
-    (function next() {
-        node.style.opacity = oppArray[x];
-        if(++x < oppArray.length) {
-            setTimeout(next, 0.001); //depending on how fast you want to fade
-        }
-    })();
-
-}
-
-function increaseTheOppacity(node)
-{
-    var oppArray = ["0.9", "0.8", "0.7", "0.6", "0.5", "0.4", "0.3", "0.2", "0.1", "0"];
-    oppArray = oppArray.reverse()
-    var x = 0;
-    (function next() {
-        node.style.opacity = oppArray[x];
-        if(++x < oppArray.length) {
-            setTimeout(next, 0.001); //depending on how fast you want to fade
-        }
-    })();
+    formSteps[currentStep].style.display = "block"
+    setTimeout(()=>{
+        formSteps[currentStep].classList.add('active')
+    }, 100)
     
+}
+
+function Fade(step){
+    setTimeout(() => {
+        formSteps[step].style.display = "none";
+    },200);
+}
+
+function getActiveCard(){
+    return formSteps.findIndex(step=>{
+        return step.classList.contains("active");
+    })
 }
